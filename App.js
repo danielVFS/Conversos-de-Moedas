e15c83d5a0c5465bc63314b79a90df05f04be493
danielVFS <danielv759153@gmail.com>
2020-06-11 14:07:07 -0300

    Estruturando pastas

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import Converter from "./src/components/Converter";
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Converter />
       </View>
     );
   }
@@ -15,5 +17,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    marginTop: 20,
   },
 });
---
+++ src/components/Converter/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class Converter extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Converter</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
